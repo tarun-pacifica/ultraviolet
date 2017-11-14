e16ae11b0136b097307469e5c6ec8898c3e986fe
Tarun Mookhey <tarun.mookhey@reinteractive.net>
2017-11-14 11:12:23 +1100

    updated

--- app/controllers/concerns/speechable.rb
+++ app/controllers/concerns/speechable.rb
@@ -2,9 +2,16 @@ require 'active_support/concern'
 module Speechable
 	extend ActiveSupport::Concern
 	def prepare_content(city,index)
+		@city = city
+		@index = index
 		hash = {'message': status}
-		hash.merge ({'warning': warning}) if @index > 1
+		hash = hash.merge({'warning': warning}) if @index > 1
+		hash = hash.merge({'warning': no_warning}) if @index < 1
 		return hash
+	end
+
+	def no_warning
+		"It's perfectly safe to go out in the sun right now"
 	end
 
 	def warning
--- app/controllers/radiations_controller.rb
+++ app/controllers/radiations_controller.rb
@@ -1,23 +1,23 @@
 class RadiationsController < ApplicationController
 	include Speechable
 	def create
-		@city = params['request']['intent']['slots']['City']['value'].downcase
-		@index = Radiation.uv_index @city
-		@content = prepare_content @city,@index
-		@response = prepare_response @content
+		@city = params['request']['intent']['slots']['City']['value']
+		@index = Radiation.uv_index(@city)
+		puts "The current index for #{@city} is #{@index} "
+		@content = prepare_content(@city,@index)
+		@response = prepare_response(@content)
 		render json: @response 
 	end
 	def prepare_response(content)
-		@message = content[:message]
-		@warning = content[:warning]
-
+		message = content[:message]
+		warning = content[:warning]
 		session_attributes = {"previous_session":"something"}
 		session_end = true
 		return {
 		      "response": {
 		        "outputSpeech": {
 		          "type": "PlainText",
-		          "text": "#{@message}', #{@warning}"
+		          "text": "#{message}, #{warning}"
 		        },
 		        "shouldEndSession": session_end
 		      },
